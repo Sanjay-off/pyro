9493b36300831fc942dbc767773d6c0093b951e7
Sanjay-off <161325708+Sanjay-off@users.noreply.github.com>
2026-01-06 14:29:17 +0530

    small changes

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,8 +1,14 @@
 from pyrogram import filters
+# from bot.client import bot_app
+
+# @bot_app.on_message(filters.command("start"))
+# async def start_handler(client, message):
+#     await message.reply_text(
+#         "Welcome! Bot started and responding."
+#     )
 from bot.client import bot_app
 
-@bot_app.on_message(filters.command("start"))
-async def start_handler(client, message):
-    await message.reply_text(
-        "Welcome! Bot started and responding."
-    )
+@bot_app.on_message(filters.text)
+async def handle_text(client, message):
+    if message.text and message.text.lower().startswith("/start"):
+        await message.reply_text("Start matched raw text")
--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from bot.client import bot_app
 import bot.handlers 
 import logging
 logging.basicConfig(level=logging.INFO)
+from pyrogram.types import BotCommand
+
 
 async def main():
     await bot_app.start()
+    await bot_app.set_bot_commands([
+        BotCommand("start", "Start the bot"),
+        BotCommand("help", "Show help")
+    ])
     print("Bot is running")
     await asyncio.Event().wait()
 
